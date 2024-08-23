29119aff34b9539db6ea67e121edd17a38d15480
sebpb <sebastianperezbertoli@gmail.com>
2024-08-23 12:16:37 -0300

    add host script to package.json

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,2 @@
-export const PORT = 5555;
 export const mongoDBURL = 
 `mongodb+srv://dummyuser:${process.env.PASSWORD}@pruebas.c76ui.mongodb.net/?retryWrites=true&w=majority&appName=Pruebas`;
--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { PORT, mongoDBURL } from "./config.js"
+import { mongoDBURL } from "./config.js"
 import mongoose from "mongoose";
 import booksRoute from "./routes/booksRoute.js"
 import cors from 'cors';
 
 const app = express();
+const PORT = process.env.PORT;
 
 // Middleware for parsing request body
 app.use(express.json());
