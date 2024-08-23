b4a6d2f06c10410fba47bfa62ca4dfff01728648
sebpb <sebastianperezbertoli@gmail.com>
2024-08-23 11:31:11 -0300

    add .env file

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555;
 export const mongoDBURL = 
-'mongodb+srv://root:root@book-store-mern.rlv4xc6.mongodb.net/?retryWrites=true&w=majority&appName=Book-Store-MERN';
+`mongodb+srv://dummyuser:${process.env.PASSWORD}@pruebas.c76ui.mongodb.net/?retryWrites=true&w=majority&appName=Pruebas`;
